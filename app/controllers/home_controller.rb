f2b5bcd11faa2716dcece0d341e0423f8d7ef92a
Seung Min <bsm7878@naver.com>
2016-12-07 07:47:49 +0000

    order 수정하기

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -197,7 +197,7 @@ class HomeController < ApplicationController
   def order #개요-관리자페이지
     @menu = Mart.find(params[:id]).menus #선택된 마트의 메뉴 정보
     mart_menu = Mart.find(params[:id]).menus.ids
-    @today_time =  Purchase.where(:menu_id => mart_menu).where(:save_time => Time.zone.now.midnight...Time.zone.now.midnight + 1.days)
+    @today_time =  Purchase.where(:menu_id => mart_menu).where(:save_time => Time.zone.now.midnight..Time.zone.now.midnight + 23.hours)
     #@today_time = Purchase.where(:menu_id => mart_menu).where("DATE(created_at) = DATE(?)", Time.now)
   end
   
