9e68beabdcff1b8bb675d30b00727be4645aeb29
Seung Min <bsm7878@naver.com>
2016-12-12 06:43:28 +0000

    웍스메일오면 주소바로 검색 및 전화걸기 기능 추가

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -8,12 +8,19 @@ class Contact < ApplicationMailer
     end
     
         
+    
     def order mart_email, title, name, menu, menu_ingredient, option, address, public_pw, want_content, phone, p1, p2, p3 #주문들어오면 알려주기
-        email = mail from: 'bsm7878@naver.com', 
-                to: mart_email, 
-                cc: 'contact@zachiyori.com',
-                subject: title,
-                body: name + "\n" + menu + "\n" + menu_ingredient + "\n" + option + "\n" + address + "\n" + public_pw + "\n" + want_content + "\n" + phone + "\n" + "\n" + p1 + "\n" + p2 + "\n" + p3
+        add = URI.encode(address)
+        naver = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&oquery=네이버지도&ie=utf8&query=#{add}"
+        RestClient.post "https://api:key-58cb05535db9e0e2095709e152b00c77"\
+        "@api.mailgun.net/v3/sandbox46f0226b110045b4a455d9119d83c8e0.mailgun.org/messages",
+        :from => "bsm7878@naver.com",
+        :to => mart_email,
+        :cc => 'contact@zachiyori.com',
+        :subject => title,
+        :html => name + "<br>" + menu + "<br>" + menu_ingredient + "<br>" + option + "<br>" + "주소:" + "<a href = #{naver}>#{address}</a>" + "<br>" + public_pw + "<br>" + want_content + "<br>" + "<a href=tel:#{phone}><u>전화걸기</u></a>" + "<br>" + "<br>" + p1 + "<br>" + p2 + "<br>" + p3
     end
+    
+    
     
 end
